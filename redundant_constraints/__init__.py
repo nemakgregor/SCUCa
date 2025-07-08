22ad340aaad910bc39f1d2ae8f56848838f14676
Grigorii Gerasimov <grigorii@MacBook-Pro-Grigorii.local>
2025-07-08 23:04:23 +0300

    save results

--- redundant_constraints/__init__.py
+++ redundant_constraints/__init__.py
@@ -1,3 +1,4 @@
 from .model import EdgeModel, GraphTrainer
 from .graph import build_grid_graph, plot_grid_graph
 from .utils import compute_dc_power_flow
+from .dataset import prepare_powerflow_dataset
